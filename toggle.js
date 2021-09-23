c77bda7b6e79e29c81017c698f7747c1001eecb0
zaivs <zaivsguitar@gmail.com>
2021-09-23 23:49:41 +0600

    initial commit

---
+++ toggle.js
@@ -0,0 +1,46 @@
+function myFunction() {
+    var element1 = document.getElementById("1");
+    element1.classList.toggle("mystyle");
+
+    var element2 = document.getElementById("2");
+    element2.classList.toggle("mystyle");
+
+    var element3 = document.getElementById("3");
+    element3.classList.toggle("mystyle");
+
+    var element4 = document.getElementById("4");
+    element4.classList.toggle("mystyle");
+
+    var element5 = document.getElementById("5");
+    element5.classList.toggle("mystyle");
+
+    var element6 = document.getElementById("6");
+    element6.classList.toggle("mystyle");
+
+    var element7 = document.getElementById("7");
+    element7.classList.toggle("mystyle");
+
+    var element8 = document.getElementById("8");
+    element8.classList.toggle("mystyle");
+
+    var element9 = document.getElementById("9");
+    element9.classList.toggle("mystyle");
+
+    var element10 = document.getElementById("10");
+    element10.classList.toggle("mystyle");
+
+    var element11 = document.getElementById("11");
+    element11.classList.toggle("mystyle");
+
+    var element12 = document.getElementById("12");
+    element12.classList.toggle("mystyle");
+
+    var element13 = document.getElementById("13");
+    element13.classList.toggle("mystyle");
+
+    var element14 = document.getElementById("14");
+    element14.classList.toggle("mystyle");
+
+    var element15 = document.getElementById("15");
+    element15.classList.toggle("mystyle");
+ }
